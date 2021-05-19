42cf5018c6328bdcc54b4dda7b0cd05a6b48efb6
Garrett Barringer <gb759819@ohio.edu>
2021-05-19 16:31:32 -0400

    First C++ code created

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Garrett Barringer
+ *        Date: 5-19-2021
+ * Description: Creating my first CS 2400 code!!
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+} //main
